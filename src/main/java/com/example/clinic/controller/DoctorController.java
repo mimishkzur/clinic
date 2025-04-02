3e56f6f88323f4b4d86916b67d100bf10cc296c2
Michael <zyuryaevm@gmail.com>
2025-04-02 21:48:43 +0300

    some actions with doctors' table

--- src/main/java/com/example/clinic/controller/DoctorController.java
+++ src/main/java/com/example/clinic/controller/DoctorController.java
@@ -1,11 +1,13 @@
 package com.example.clinic.controller;
 
+import com.example.clinic.model.Doctor;
 import com.example.clinic.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/doctors")
@@ -21,6 +23,21 @@ public class DoctorController {
     @GetMapping
     public String listDoctors(Model model) {
         model.addAttribute("doctors", doctorRepository.findAll());
+        model.addAttribute("newDoctor", new Doctor());
         return "doctors/list";
     }
+
+    @GetMapping("/new")
+    public String showAddForm(Model model) {
+        model.addAttribute("doctor", new Doctor());
+        return "doctors/add-form";
+    }
+
+    @PostMapping("/add")
+    public String addDoctor(@ModelAttribute Doctor new_doctor) {
+        doctorRepository.save(new_doctor);
+        return "redirect:/doctors";
+    }
+
+//    @PostMapping("/")
 }
