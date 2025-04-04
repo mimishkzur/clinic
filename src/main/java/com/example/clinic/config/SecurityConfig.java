1320bfa7f64b08e0000a5784d79e56b461f05f04
Michael <zyuryaevm@gmail.com>
2025-04-04 15:52:13 +0300

    again some actions (starting SecurityConfig)

---
+++ src/main/java/com/example/clinic/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.clinic.config;
+
+public class SecurityConfig {
+
+
+}
--- src/main/java/com/example/clinic/controller/MainController.java
+++ src/main/java/com/example/clinic/controller/MainController.java
@@ -1,13 +1,26 @@
 package com.example.clinic.controller;
 
+import com.example.clinic.model.Doctor;
+import com.example.clinic.repository.DoctorRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private final DoctorRepository doctorRepository;
+
+    public MainController(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<Doctor> doctors = doctorRepository.findAll();
+        model.addAttribute("doctors", doctors);
         return "home";
     }
 
@@ -15,4 +28,6 @@ public class MainController {
     public String about() {
         return "about";
     }
+
+
 }
